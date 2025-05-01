a0277eb0fb80016fd3a251c129e86c5f84c53b97
Jagteshwar <jagteshwars3@gmail.com>
2025-04-30 22:07:55 -0400

    launch and async await

--- app/src/main/java/com/jagteshwar/coroutines/MainActivity.kt
+++ app/src/main/java/com/jagteshwar/coroutines/MainActivity.kt
@@ -1,41 +1,20 @@
 package com.jagteshwar.coroutines
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.jagteshwar.coroutines.ui.theme.CoroutinesTheme
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.yield
 
 const val TAG: String = "KOTLIN"
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -45,24 +24,34 @@ class MainActivity : ComponentActivity() {
             }
         }
         CoroutineScope(Dispatchers.Main).launch {
-            task1()
+            printFollowers()
         }
 
-        CoroutineScope(Dispatchers.Main).launch {
-            task2()
-        }
+
     }
-}
 
-suspend fun task1() {
-    Log.d(TAG, "STARTING OF TASK 1")
-    yield()
-    Log.d(TAG, "ENDING OF TASK 1")
-}
+    private suspend fun printFollowers() {
+        var fbFollowers = 0
+       val job =  CoroutineScope(Dispatchers.IO).launch {
+            fbFollowers = getFbFollowers()
+        }
+        job.join()
+        Log.d(TAG, "fb followers: $fbFollowers")
 
-suspend fun task2() {
-    Log.d(TAG, "STARTING OF TASK 2")
-    yield()
-    Log.d(TAG, "ENDING OF TASK 2")
+        val insta = CoroutineScope(Dispatchers.IO).async {
+            getInstaFollowers()
+        }
+        Log.d(TAG, "insta followers: ${insta.await()}")
+
+    }
+
+    private suspend fun getFbFollowers(): Int {
+        delay(1000)
+        return 54
+    }
+    private suspend fun getInstaFollowers(): Int {
+        delay(1000)
+        return 99
+    }
 }
 
